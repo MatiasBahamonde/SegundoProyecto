51c15514351a7aa5a554a0d70aec4b864b864923
MatiasBahamonde <matiasalejandro.bahamonde@alumnos.ulagos.cl>
2024-11-05 10:54:04 -0300

    Update ProductList.jsx

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,14 +1,16 @@
 
 
 
+
+
+
+import React from 'react';
 import ProductItem from '../ProductItem/ProductItem'; 
 import PropTypes from 'prop-types';
 
 const ProductList = ({ products, addToCart, onSelectProduct }) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 -mt-40"> {
-
-    }
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 -mt-40">
       {products.map((product) => (
         <ProductItem 
           key={product.id} 
@@ -21,8 +23,18 @@ const ProductList = ({ products, addToCart, onSelectProduct }) => {
   );
 };
 
+
 ProductList.propTypes = {
-  products: PropTypes.array.isRequired,
+  products: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      price: PropTypes.number.isRequired,
+      image: PropTypes.string.isRequired,
+      category: PropTypes.string.isRequired,
+    })
+  ).isRequired,
   addToCart: PropTypes.func.isRequired,
   onSelectProduct: PropTypes.func.isRequired,
 };
